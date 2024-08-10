6a6e14e12f45d5f80a1a8324aae32b8401920f6d
furkan aydin <furknn2112@gmail.com>
2024-08-10 18:32:37 +0300

    feat(order): Save persisting changes of CreateOrderCommandHandler
    - Add IOrderRepository and IUnitOfWork dependencies to handle method
    - Implement saving of Order and OrderItems using repository and unit of work

--- src/Services/Order/Order.Application/Orders/Commands/CreateOrderCommand.cs
+++ src/Services/Order/Order.Application/Orders/Commands/CreateOrderCommand.cs
@@ -7,14 +7,18 @@ namespace Order.Application;
 public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
 
 public record CreateOrderResult(Guid Id);
-public class CreateOrderCommandHandler()
+public class CreateOrderCommandHandler(IOrderRepository orderRepository, IUnitOfWork unitOfWork)
     : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
     public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
     {
-        var address = Address.Of(request.Order.Address.FirstName,request.Order.Address.LastName,request.Order.Address.Country);
+        var address = Address.Of(request.Order.Address.FirstName, request.Order.Address.LastName, request.Order.Address.Country);
 
-        var order = Domain.Order.Create(Guid.NewGuid(), request.Order.CustomerId,address);
+        var order = Domain.Order.Create(Guid.NewGuid(), request.Order.CustomerId, address);
+        request.Order.OrderItems.ForEach(x => order.AddOrderItem(Guid.NewGuid(), x.ProductId, x.Quantity, x.Price));
+
+        orderRepository.Create(order);
+        await unitOfWork.SaveChangesAsync();
 
         return new CreateOrderResult(order.Id);
     }
