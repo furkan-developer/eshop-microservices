21ff0ac92a57151d1d651178ca2d64712c12f26e
furkan aydin <furknn2112@gmail.com>
2024-08-14 16:40:53 +0300

    feat(order.application): Register MediatR's services to DI

--- src/Services/Order/Order.Application/ApplicationServiceExtensions.cs
+++ src/Services/Order/Order.Application/ApplicationServiceExtensions.cs
@@ -6,6 +6,10 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services){
 
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(typeof(ApplicationServiceExtensions).Assembly);
+        });
+
         return services;
     }
 }
