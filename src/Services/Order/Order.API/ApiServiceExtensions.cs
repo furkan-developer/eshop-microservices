0c0bf86762e91882546c4a63f6882079b8977e3e
furkan aydin <furknn2112@gmail.com>
2024-08-14 16:57:42 +0300

    feat(order.api): Add '/' post endpoint to create order

--- src/Services/Order/Order.API/ApiServiceExtensions.cs
+++ src/Services/Order/Order.API/ApiServiceExtensions.cs
@@ -1,16 +1,29 @@
-﻿namespace Order.API;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Order.Application;
+
+namespace Order.API;
 
 public static class ApiServiceExtensions
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration){
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
 
         return services;
     }
 
-    public static WebApplication HandleRequestPipeline(this WebApplication app){
-
+    public static WebApplication HandleRequestPipeline(this WebApplication app)
+    {
         app.MapGet("/", () => "Hello World!");
 
+        app.MapPost("/", async ([FromServices] ISender sender) =>
+        {
+            var result = await sender.Send(new CreateOrderCommand(new OrderDto(Guid.NewGuid(), Guid.NewGuid(), "OrderName", Domain.OrderStatus.Pending, new AddressDto("name", "surname", "country"), new List<OrderItemDto>() { new OrderItemDto(Guid.NewGuid(), 10, 100) })));
+
+            return Results.Ok(new { id = result.Id });
+        });
+
         return app;
-    } 
+    }
 }
